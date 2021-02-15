385932ac038d5888b84669a3d1dbd49e93ff8a16
fenixfoux <dima999.10@mail.ru>
2021-02-15 17:07:16 +0200

    first and second button

--- js.js
+++ js.js
@@ -20,13 +20,113 @@ $(document).ready(function(){
 		//detected_input_range();
 		if(a>=100&&a<200){
 			difference_range = a-100;
-			stable_range = 1494;
 			range_height_stable = 9; 
-			range_height_variable = (range_height_variable*9)/100;
+			stable_range = 1494;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
 			variable_range = ((stable_range-1384)/100)*difference_range; 
 			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=200&&a<300){
+			difference_range = a-200;
+			range_height_stable = 20; 
+			stable_range = 1384;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1260)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=300&&a<400){
+			difference_range = a-300;
+			range_height_stable = 36; 
+			stable_range = 1260;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1105)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
 		}else {
-			alert("more than 200");
+			alert("тип снаряда Charge_0 не может лететь дальше 400метров");
+		}
+		console.log(calc_result_range);
+	});
+
+	$('#calc_range_1').click(function(){ 
+		a = parseInt(document.getElementById('dist').value);  
+		if (isNaN(a)==true) a=0; 
+		range_height_variable = parseInt(document.getElementById('dist_height').value);  
+		if (isNaN(b)==true) b=0; 
+		var result = a ; 
+		var result2 = b;
+		document.getElementById('result').innerHTML = result;
+		document.getElementById('result2').innerHTML = result2; 
+		if(a>=300&&a<400){
+			difference_range = a-300;
+			range_height_stable = 4; 
+			stable_range = 1488;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1450)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=400&&a<500){
+			difference_range = a-400;
+			range_height_stable = 5; 
+			stable_range = 1450;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1410)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=500&&a<600){
+			difference_range = a-500;
+			range_height_stable = 7; 
+			stable_range = 1410;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1369)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=600&&a<700){
+			difference_range = a-600;
+			range_height_stable = 8; 
+			stable_range = 1369;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1327)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=700&&a<800){
+			difference_range = a-700;
+			range_height_stable = 10; 
+			stable_range = 1327;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1282)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=800&&a<900){
+			difference_range = a-800;
+			range_height_stable = 13; 
+			stable_range = 1282;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1234)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=900&&a<1000){
+			difference_range = a-900;
+			range_height_stable = 16; 
+			stable_range = 1234;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1182)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=1000&&a<1100){
+			difference_range = a-1000;
+			range_height_stable = 20; 
+			stable_range = 1182;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1123)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=1100&&a<1200){
+			difference_range = a-1100;
+			range_height_stable = 26; 
+			stable_range = 1123;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-1053)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}else if(a>=1200&&a<1300){
+			difference_range = a-1200;
+			range_height_stable = 36; 
+			stable_range = 1053;
+			range_height_variable = (range_height_variable*range_height_stable)/100;
+			variable_range = ((stable_range-960)/100)*difference_range; 
+			calc_result_range=stable_range-(range_height_variable+variable_range);
+		}
+		else {
+			alert("тип снаряда Charge_0 не может лететь дальше 400метров");
 		}
 		console.log(calc_result_range);
 	});
@@ -34,11 +134,14 @@ $(document).ready(function(){
 //потом результат делим на 100 чтобы определить сколько за каждый 1метр нужно 
 //прибавить к минусуемому числу из дальности текущего диапазона
 
+
+/*
+
 	function detected_input_range(a){
 		if(a>=100&&a<200){
 			difference_range = a-100;
-			stable_range = 1494;
 			range_height_stable = 9; 
+			stable_range = 1494;
 			range_height_variable = (range_height_variable*9)/100;
 			variable_range = ((stable_range-1384)/100)*difference_range; 
 			calc_result_range=stable_range-(range_height_variable+variable_range);
@@ -46,8 +149,10 @@ $(document).ready(function(){
 			difference_range = a-200; 
 			stable_range = 1384;
 		}else if(a>=300&&a<400){ 
+			difference_range = a-300; 
 			stable_range = 1260;
 		}else if(a>=400&&a<500){ 
+			difference_range = a-400; 
 			stable_range = 1105;
 		}
 		return(variable_range);
